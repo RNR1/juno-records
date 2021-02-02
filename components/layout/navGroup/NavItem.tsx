3d9dd1d8c2828d9bdad9b376b5a74a781e2562a6
Ron Braha <ron.braha@icloud.com>
2021-02-03 00:01:13 +0200

    feat(navbar): improve hover lists

--- components/layout/navGroup/NavItem.tsx
+++ components/layout/navGroup/NavItem.tsx
@@ -1,15 +1,22 @@
-import { FC } from 'react'
+import { FC, useRef, useState } from 'react'
 import Link from 'next/link'
 import { IconProp } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
-import styled from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 import List from 'components/shared/List'
 import genres from 'data/genres.json'
 import useWindowWidth from 'hooks/useWindowWidth'
-import useClickOutside from 'hooks/useClickOutside'
+
 import FlagIcon from './FlagIcon'
 import StatusIndicator from './StatusIndicator'
+
+interface Selection {
+  label: string
+  position: string
+  width: number
+  href: string
+}
 
 export interface Item {
   icon?: IconProp | string | string[]
@@ -19,7 +26,7 @@ export interface Item {
   status?: 'online' | 'n/a' | 'offline'
   short?: string
   to: string
-  hoverList?: { label: string }[]
+  hoverList?: { label: string; selection: Selection[] }[]
   gridList?: { label: string; position: string; width: number }[]
 }
 
@@ -40,13 +47,22 @@ const NavItem: FC<NavItemProps> = ({
   const width = useWindowWidth()
   const largeScreen = width >= 1024 || !responsive
   const smallScreen = width < 575
-  const { ref, open, toggle } = useClickOutside()
+  const [open, setIsOpen] = useState<boolean>()
+  const wrapperRef = useRef<HTMLDivElement | null>(null)
+  const [selection, setSelection] = useState<Selection[]>(
+    hoverList?.[0].selection || []
+  )
 
   if (smallScreen && label === 'Wishlist') return null
   return (
-    <Wrapper ref={ref} hasHover={hoverList?.length! > 0 && open}>
+    <Wrapper
+      ref={wrapperRef}
+      onMouseEnter={() => setIsOpen(true)}
+      onMouseLeave={() => setIsOpen(false)}
+      hasHover={hoverList?.length! > 0 && open}
+    >
       <Link href={to}>
-        <Container onClick={toggle} {...linkProps}>
+        <Container {...linkProps}>
           {iso && <FlagIcon iso={iso} />}
           {icon && (
             <FontAwesomeIcon
@@ -64,9 +80,9 @@ const NavItem: FC<NavItemProps> = ({
         </Container>
       </Link>
       {open && hoverList && (
-        <HoverMenu>
+        <HoverMenu onMouseLeave={() => setIsOpen(false)}>
           {gridList && (
-            <GridList $width={150 * (gridList.length / 2)}>
+            <GridList $columns={5} $item={150} $width={750}>
               {gridList.map(item => (
                 <GridListItem key={item.label}>
                   <GridImage $position={item.position} $width={item.width} />
@@ -78,13 +94,36 @@ const NavItem: FC<NavItemProps> = ({
           <ListStack>
             <CustomList>
               {hoverList.map(item => (
-                <ItemStack key={item.label}>
+                <ItemStack
+                  onMouseEnter={() => setSelection(item.selection)}
+                  key={item.label}
+                >
                   <ListItem>{item.label}</ListItem>
                   <FontAwesomeIcon icon='angle-right' size='sm' />
                 </ItemStack>
               ))}
             </CustomList>
             <CustomList>
+              {selection && (
+                <GridList
+                  className='fade'
+                  $noShadow
+                  $columns={3}
+                  $item={156.8}
+                  $width={470}
+                >
+                  {selection?.map(item => (
+                    <GridListItem key={item.label}>
+                      <GridImage
+                        $height={50}
+                        $position={item.position}
+                        $width={item.width}
+                      />
+                      {item.label}
+                    </GridListItem>
+                  ))}
+                </GridList>
+              )}
               {label === 'Music' &&
                 genres.map(genre => (
                   <div key={genre.label}>
@@ -125,11 +164,15 @@ const Wrapper = styled.div<{ hasHover?: boolean }>`
 
 const Container = styled.a`
   cursor: pointer;
+
+  &:hover {
+  }
 `
 
 const CustomList = styled(List)`
   position: relative;
   margin: 0;
+  padding: 0;
 `
 
 const ListStack = styled.div`
@@ -166,13 +209,37 @@ const InnerItemStack = styled(ItemStack)`
   }
 `
 
-const GridList = styled(List)<{ $width: number }>`
+const fade = keyframes`
+  0% {
+    opacity: 0;
+  } 
+  100% {
+    opacity: 1;
+  }
+`
+
+const GridList = styled(List)<{
+  $columns: number
+  $item: number
+  $width: number
+  $noShadow?: true
+}>`
+  ${({ $noShadow }) =>
+    $noShadow &&
+    css`
+      box-shadow: none;
+    `}
   position: static;
-  display: flex;
-  flex-wrap: wrap;
-  width: ${({ $width }) => $width}px;
+  display: grid;
+  grid-template-columns: ${({ $item, $columns }) =>
+    `repeat(${$columns}, ${$item}px)`};
+  grid-template-rows: auto;
   margin: 0;
   padding: 0;
+  animation-duration: 0.3s;
+  animation-name: ${fade};
+  animation-iteration-count: 1;
+  animation-direction: alternate;
 `
 
 const GridListItem = styled.li`
@@ -188,10 +255,14 @@ const GridListItem = styled.li`
   }
 `
 
-const GridImage = styled.div<{ $position: string; $width: number }>`
+const GridImage = styled.div<{
+  $position: string
+  $width: number
+  $height?: number
+}>`
   background-image: url('https://wwwcdn.juno.co.uk/12090200/images/jr-sprite-nav.png');
   display: block;
-  height: 60px;
+  height: ${({ $height = 60 }) => $height}px;
   margin: 0 auto 7px auto;
   background-position: ${({ $position }) => $position};
   width: ${({ $width }) => $width}px;
